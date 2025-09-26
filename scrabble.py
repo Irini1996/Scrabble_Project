1ed4e1941a881dcbd0323c006e857c31f3793b8a
Irini1996 <iringega@gmail.com>
2025-09-26 22:38:57 +0200

    Add files via upload

---
+++ scrabble.py
@@ -0,0 +1,25 @@
+# https://github.com/jmlewis/valett/blob/master/scrabble/sowpods.txt
+# TABIND Scrabble 
+# The objective of this project is to develop a program that takes the letter combination “tabind” only once and creates an alphabetical list of all the words, from a Scrabble dictionary that can be found with those letters. 
+
+from collections import Counter
+
+letters = "tabind"  # the letters we want to use
+bag = Counter(letters)
+
+
+with open("dict.txt", "r", encoding="utf-8") as f:
+    words = [w.strip().lower() for w in f if w.strip()]
+
+# Keep only words that:
+# (1) are at least 2 letters long
+# (2) can be formed with the available letters in bag
+valid = [w for w in words if len(w) >= 2 and not (Counter(w) - bag)]
+
+
+# Sort valid words alphabetically
+valid.sort()
+
+print(f"Found {len(valid)} words with the letters '{letters}':\n")
+for w in valid:
+    print(w)
